bfa35a4a447813d95e041112e747e1258e44d53e
Hiroshi Miura <miurahr@linux.com>
2018-09-12 11:25:16 +0900

    cmake: retrieve soversion from GDALmake.opt.in
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- cmake/helpers/GdalVersion.cmake
+++ cmake/helpers/GdalVersion.cmake
@@ -30,7 +30,7 @@
 #   the sha of the latest commit in it. And replace GDAL_RELEASE_DATE with the
 #   date of the commit.
 # parse the version number from gdal_version.h and include in GDAL_MAJOR_VERSION and GDAL_MINOR_VERSION
-file(READ ${CMAKE_CURRENT_SOURCE_DIR}/gcore/gdal_version.h.in GDAL_VERSION_H_CONTENTS)
+file(READ ${CMAKE_SOURCE_DIR}/gdal/gcore/gdal_version.h.in GDAL_VERSION_H_CONTENTS)
 string(REGEX MATCH "GDAL_VERSION_MAJOR[ \t]+([0-9]+)"
   GDAL_VERSION_MAJOR ${GDAL_VERSION_H_CONTENTS})
 string (REGEX MATCH "([0-9]+)"
@@ -58,7 +58,7 @@ else()
         git_local_changes(GIT_LOCAL_CHG)
         string(SUBSTRING ${GDAL_GIT_HASH} 0 10 REV)
             set(GDAL_DEV_REVISION "dev-${REV}")
-        file(READ ${CMAKE_CURRENT_SOURCE_DIR}/gcore/gdal_version.h.in GDAL_VERSION_H_CONTENTS)
+        file(READ ${CMAKE_SOURCE_DIR}/gdal/gcore/gdal_version.h.in GDAL_VERSION_H_CONTENTS)
         string(CONCAT
                GDAL_VERSION_H_CONTENTS
                "/* This is a generated file from gdal_version.h.in. DO NOT MODIFY !!!! */\n"
@@ -71,7 +71,7 @@ else()
              OUTPUT  ${CMAKE_CURRENT_BINARY_DIR}/gcore/gdal_version.h
              CONTENT "${GDAL_VERSION_H_CONTENTS}")
     else()
-        file(READ ${CMAKE_CURRENT_SOURCE_DIR}/gcore/gdal_version.h.in GDAL_VERSION_H_CONTENTS)
+        file(READ ${CMAKE_SOURCE_DIR}/gdal/gcore/gdal_version.h.in GDAL_VERSION_H_CONTENTS)
         string(CONCAT
                GDAL_VERSION_H_CONTENTS
                "/* This is a generated file from gdal_version.h.in. DO NOT MODIFY !!!! */\n"
@@ -82,6 +82,17 @@ else()
     endif()
 endif()
 
+if(NOT GDAL_SOVERSION)
+    file(READ ${CMAKE_SOURCE_DIR}/gdal/GDALmake.opt.in GDAL_MAKE_OPT_CONTENT)
+    string(REGEX MATCH "^LIBGDAL_CURRENT :=[ /t]+([0-9]+)$" "${GDAL_MAKE_OPT_CONTENT}" GDAL_SOVERSION)
+    string(REGEX MATCH "([0-9]+)" "${GDAL_SOVERSION}" GDAL_SOVERSION)
+    # when fails to get soversion, fallback to some default
+    if(NOT GDAL_SOVERSION)
+        set(GDAL_SOVERSION "${GDAL_VERSION_MAJOR}${GDAL_VERSION_MINOR}")
+    endif()
+endif()
+
 # Setup package meta-data
 set(GDAL_VERSION ${GDAL_VERSION_MAJOR}.${GDAL_VERSION_MINOR}.${GDAL_VERSION_REV}${GDAL_DEV_REVISION})
 message(STATUS "gdal version=[${GDAL_VERSION}]")
+message(STATUS "SO version=${GDAL_SOVERSION}")
