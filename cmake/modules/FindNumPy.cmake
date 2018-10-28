f5efefc8a185b9d96b13caa04b8418927ffa9c6d
Hiroshi Miura <miurahr@linux.com>
2018-10-29 07:42:43 +0900

    cmake: Check NumPy version
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- cmake/modules/FindNumPy.cmake
+++ cmake/modules/FindNumPy.cmake
@@ -1,18 +1,21 @@
-# Find the Python NumPy package
-# Python_NUMPY_INCLUDE_DIR
-# Python_NUMPY_FOUND
-# will be set by this script
+## (re)Distributed under the OSI-approved 3-Clause BSD License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
 
-cmake_minimum_required(VERSION 3.5)
+#.rst:
+# FindNumPy
+# --------
+#
+# Copyright (c) 2018 Hiroshi Miura
 
-  if(NOT Python_FOUND)
+
+if(NOT Python_FOUND)
     if(NumPy_FIND_QUIETLY)
-      find_package(Python QUIET)
+        find_package(Python QUIET)
     else()
-      find_package(Python)
-      set(__numpy_out 1)
+        find_package(Python)
+        set(__numpy_out 1)
     endif()
-  endif()
+endif()
 
 if (Python_EXECUTABLE)
   # Find out the include path
@@ -24,7 +27,7 @@ if (Python_EXECUTABLE)
   execute_process(
     COMMAND "${Python_EXECUTABLE}" -c
             "from __future__ import print_function\ntry: import numpy; print(numpy.__version__, end='')\nexcept:pass\n"
-    OUTPUT_VARIABLE __numpy_version)
+    OUTPUT_VARIABLE NumPy_VERSION)
 elseif(__numpy_out)
   message(STATUS "Python executable not found.")
 endif()
@@ -36,6 +39,24 @@ if(NumPy_INCLUDE_DIR)
   set(NumPy_FOUND 1 CACHE INTERNAL "Python numpy found")
 endif()
 
+string(REGEX MATCH "^[0-9]+\\.[0-9]+\\.[0-9]+" _VER_CHECK "${NumPy_VERSION}")
+if("${_VER_CHECK}" STREQUAL "")
+  message(FATAL_ERROR
+          "Requested version and include path from NumPy, got instead:\n${NUMPY_VERSION}\n")
+  return()
+endif()
+
+# Get the major and minor version numbers
+string(REGEX REPLACE "\\." ";" _NUMPY_VERSION_LIST ${NumPy_VERSION})
+list(GET _NUMPY_VERSION_LIST 0 NUMPY_VERSION_MAJOR)
+list(GET _NUMPY_VERSION_LIST 1 NUMPY_VERSION_MINOR)
+list(GET _NUMPY_VERSION_LIST 2 NUMPY_VERSION_PATCH)
+string(REGEX MATCH "[0-9]*" NUMPY_VERSION_PATCH ${NUMPY_VERSION_PATCH})
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(NumPy REQUIRED_VARS NumPy_INCLUDE_DIR
-                                        VERSION_VAR __numpy_version)
+                                        VERSION_VAR NumPy_VERSION)
+
+if(NumPy_FOUND)
+    set(NumPy_INCLUDE_DIRS ${NumPy_INCLUDE_DIR})
+endif()
