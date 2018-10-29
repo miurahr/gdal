e5c40af70de86a55cdf146b254271eea04d7ba75
Hiroshi Miura <miurahr@linux.com>
2018-10-29 19:55:40 +0900

    cmake: update rasterlite2 and spatialite detection and flags
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- cmake/modules/FindRasterlite2.cmake
+++ cmake/modules/FindRasterlite2.cmake
@@ -1,71 +1,42 @@
-# Find Rasterlite
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file COPYING-CMAKE-SCRIPTS or https://cmake.org/licensing for details.
+
+#.rst
+# Find Rasterlite2
 # ~~~~~~~~~~~~~~~
+#
+# CMake module to search for rasterlite library
 #
 # Copyright (c) 2009, Sandro Furieri <a.furieri at lqt.it>
 # Copyright (C) 2017,2018 Hiroshi Miura
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
-# CMake module to search for rasterlite library
 #
 # If it's found it sets RASTERLITE2_FOUND to TRUE
 # and following variables are set:
 #    RASTERLITE2_INCLUDE_DIR
 #    RASTERLITE2_LIBRARIES
 #    RASTERLITE2_VERSION_STRING
-#
-# FIND_PATH and FIND_LIBRARY normally search standard locations
-# before the specified paths. To search non-standard paths first,
-# FIND_* is invoked first with specified paths and NO_DEFAULT_PATH
-# and then again with no specified paths to search the default
-# locations. When an earlier FIND_* succeeds, subsequent FIND_*s
-# searching for the same item do nothing. 
-
-# try to use sqlite framework on mac
-# want clean framework path, not unix compatibility path
-if(APPLE)
-    if(CMAKE_FIND_FRAMEWORK MATCHES "FIRST"
-      OR CMAKE_FRAMEWORK_PATH MATCHES "ONLY"
-      OR NOT CMAKE_FIND_FRAMEWORK)
-        set(CMAKE_FIND_FRAMEWORK_save ${CMAKE_FIND_FRAMEWORK} CACHE STRING "" FORCE)
-        set(CMAKE_FIND_FRAMEWORK "ONLY" CACHE STRING "" FORCE)
-        find_path(RASTERLITE2_INCLUDE_DIR rasterlite.h)
-        # if no spatialite header, we don't want sqlite find below to succeed
-        if(RASTERLITE2_INCLUDE_DIR)
-            find_library(RASTERLITE2_LIBRARY rasterlite)
-            # FIND_PATH doesn't add "Headers" for a framework
-            set(RASTERLITE_INCLUDE_DIR ${RASTERLITE2_LIBRARY}/Headers CACHE PATH "Path to a file." FORCE)
-        endif()
-        set(CMAKE_FIND_FRAMEWORK ${CMAKE_FIND_FRAMEWORK_save} CACHE STRING "" FORCE)
-    endif()
-endif()
 
 find_package(PkgConfig QUIET)
 if(PKG_CONFIG_FOUND)
-    # try using pkg-config to get the directories and then use these values
-    # in the FIND_PATH() and FIND_LIBRARY() calls
-    PKG_CHECK_MODULES(PC_RASTERLITE2 QUIET rasterlite2)
-    SET(RASTERLITE2_VERSION_STRING ${PC_RASTERLITE2_VERSION} CACHE INTERNAL "")
+    pkg_check_modules(PC_RASTERLITE2 QUIET rasterlite2)
+    set(RASTERLITE2_VERSION_STRING ${PC_RASTERLITE2_VERSION} CACHE INTERNAL "")
 endif()
 
-FIND_PATH(RASTERLITE2_INCLUDE_DIR
+find_path(RASTERLITE2_INCLUDE_DIR
           NAMES rasterlite2.h
           SUFFIX_PATHS rasterlite2
           HINSTS ${PC_RASTERLITE2_INCLUDE_DIR})
-FIND_LIBRARY(RASTERLITE2_LIBRARY
+find_library(RASTERLITE2_LIBRARY
              NAMES rasterlite2
              HINTS ${PC_RASTERLITE2_LIBRARY})
-
 mark_as_advanced(RASTERLITE2_LIBRARY RASTERLITE2_INCLUDE_DIR)
 
-# Handle the QUIETLY and REQUIRED arguments and set RASTERLITE2_FOUND to TRUE
-# if all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(RASTERLITE2
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(RASTERLITE2
                                   FOUND_VAR RASTERLITE2_FOUND
                                   REQUIRED_VARS RASTERLITE2_LIBRARY RASTERLITE2_INCLUDE_DIR
                                   VERSION_VAR RASTERLITE2_VERSION_STRING)
-
 if(RASTERLITE2_FOUND)
     set(RASTERLITE2_LIBRARIES ${RASTERLITE2_LIBRARY})
     set(RASTERLITE2_INCLUDE_DIRS ${RASTERLITE2_INCLUDE_DIR})
