817eb106709f11a6c458135660dc2542b72f92ff
Hiroshi Miura <miurahr@linux.com>
2018-11-03 22:20:22 +0900

    Vagrant; no verbose download cmake 3.12
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- gdal/scripts/vagrant/install-cmake-3.12.sh
+++ gdal/scripts/vagrant/install-cmake-3.12.sh
@@ -6,7 +6,7 @@ CMAKEVER=3.12
 CMAKEREV=3
 
 echo "Downloading cmake binary package version ${CMAKEVER}.${CMAKEREV}..."
-wget -nc -c -P /var/cache/wget https://cmake.org/files/v${CMAKEVER}/cmake-${CMAKEVER}.${CMAKEREV}-Linux-x86_64.tar.gz
+wget -nv -nc -c -P /var/cache/wget https://cmake.org/files/v${CMAKEVER}/cmake-${CMAKEVER}.${CMAKEREV}-Linux-x86_64.tar.gz
 cat /var/cache/wget/cmake-${CMAKEVER}.${CMAKEREV}-Linux-x86_64.tar.gz | (cd /opt; sudo tar xzf -)
 if [ -e /opt/cmake ]; then
     sudo rm -f /opt/cmake
