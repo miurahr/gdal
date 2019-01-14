9547ad6d19017949248146f4ae942692501e538e
Hiroshi Miura <miurahr@linux.com>
2019-01-14 21:11:25 +0900

    cmake: Update FindOGDI
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- cmake/modules/packages/FindOGDI.cmake
+++ cmake/modules/packages/FindOGDI.cmake
@@ -23,18 +23,17 @@ if(OGDI_INCLUDE_DIR AND OGDI_LIBRARY)
     )
 endif()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OGDI
-    REQUIRED_VARS OGDI_LIBRARY OGDI_INCLUDE_DIR
-    VERSION_VAR OGDI_VERSION
-)
+find_package_handle_standard_args(OGDI REQUIRED_VARS OGDI_LIBRARY OGDI_INCLUDE_DIR
+                                  VERSION_VAR OGDI_VERSION)
 
 if(OGDI_FOUND)
     set(OGDI_LIBRARIES ${OGDI_LIBRARY})
     set(OGDI_INCLUDE_DIRS ${OGDI_INCLUDE_DIR})
     if(NOT TARGET OGDI::OGDI)
         add_library(OGDI::OGDI UNKNOWN IMPORTED)
-        set_target_properties(OGDI::OGDI PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${OGDI_INCLUDE_DIR}")
-        set_property(TARGET OGDI::OGDI APPEND PROPERTY IMPORTED_LOCATION "${OGDI_LIBRARY}")
+        set_target_properties(OGDI::OGDI PROPERTIES
+                              INTERFACE_INCLUDE_DIRECTORIES "${OGDI_INCLUDE_DIR}"
+                              IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                              IMPORTED_LOCATION "${OGDI_LIBRARY}")
     endif()
 endif()
