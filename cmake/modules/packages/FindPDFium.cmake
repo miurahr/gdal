52388fe92510635575e481435fab6326b13e7adb
Hiroshi Miura <miurahr@linux.com>
2019-01-18 10:12:00 +0900

    cmake: Improve pdf driver and Find packages
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- cmake/modules/packages/FindPDFium.cmake
+++ cmake/modules/packages/FindPDFium.cmake
@@ -1,57 +1,65 @@
-# Find PDFium
-# ~~~~~~~~~
-#
-# Copyright (c) 2017, Hiroshi Miura <miurahr@linux.com>
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-#
-# If it's found it sets PDFIUM_FOUND to TRUE
-# and following variables are set:
-#    PDFIUM_INCLUDE_DIR
-#    PDFIUM_LIBRARY
-#    PDFIUM_VERSION
-#
-# FIND_PATH and FIND_LIBRARY normally search standard locations
-# before the specified paths. To search non-standard paths first,
-# FIND_* is invoked first with specified paths and NO_DEFAULT_PATH
-# and then again with no specified paths to search the default
-# locations. When an earlier FIND_* succeeds, subsequent FIND_*s
-# searching for the same item do nothing.
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file COPYING-CMAKE-SCRIPTS or https://cmake.org/licensing for details.
+
+#[=======================================================================[.rst:
+FindPDFium
+-----------
+
+Copyright (c) 2017, Hiroshi Miura <miurahr@linux.com>
+
+Find the PDFium headers and libraries.
+
+IMPORTED Targets
+^^^^^^^^^^^^^^^^
+
+This module defines :prop_tgt:`IMPORTED` target ``PDFIUM::PDFium``, if
+PDFium has been found.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This module defines the following variables:
+
+``PDFIUM_FOUND``
+  True if PDFium found.
+
+``PDFIUM_INCLUDE_DIRS``
+  where to find header files.
+
+``PDFIUM_LIBRARIES``
+  List of PDFium libraries to link.
+
+``PDFIUM_VERSION_STRING``
+  Version of PDFium library.
+
+#]=======================================================================]
 
 find_package(PkgConfig QUIET)
 if(PKG_CONFIG_FOUND)
     pkg_check_modules(PC_PDFIUM QUIET pdfium)
 endif()
 
-set(PDFIUM_INCLUDE_PATHS
-        "$ENV{LIB_DIR}/"
-        "$ENV{LIB_DIR}/include/"
-        /usr/include/pdfium
-        /usr/local/include/pdfium
-        #mingw
-        c:/msys/local/include/pdfium
-)
-
-set(PDFIUM_LIBRARIES_PATHS
-      "$ENV{LIB_DIR}/lib"
-      /usr/lib
-      /usr/local/lib
-      #mingw
-      c:/msys/local/lib
-)
-
 find_path(PDFIUM_INCLUDE_DIR
-        NAMES POFDoc.h
-        PATHS ${PDFIUM_INCLUDE_PATHS}
-        HINTS ${PC_PDFIUM_INCLUDE_DIRS}
-)
+          NAMES POFDoc.h
+          HINTS ${PC_PDFIUM_INCLUDE_DIRS}
+          PATH_SUFFIXES pdfium)
 
 find_library(PDFIUM_LIBRARY
-        NAMES pdfium libpdfium
-        PATHS ${PDFIUM_LIBRARIES_PATHS}
-        HINTS ${PC_PDFIUM_LIBRARY_DIRS}
-)
+             NAMES pdfium libpdfium
+             HINTS ${PC_PDFIUM_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(PDFIUM DEFAULT_MSG PDFIUM_LIBRARY PDFIUM_INCLUDE_DIR)
+find_package_handle_standard_args(PDFIUM
+                                  REQUIRED_VARS PDFIUM_LIBRARY PDFIUM_INCLUDE_DIR)
 
+if(PDFIUM_FOUND)
+  set(PDFIUM_INCLUDE_DIRS ${PDFIUM_INCLUDE_DIR})
+  set(PDFIUM_LIBRARIES ${PDFIUM_LIBRARY})
+  if(NOT TARGET PDFIUM:PDFium)
+    add_library(PDFIUM::PDFium UNKNOWN IMPORTED)
+    set_target_properties(PDFIUM::PDFium PROPERTIES
+                          INTERFACE_INCLUDE_DIRECTORIES ${PDFIUM_INCLUDE_DIR}
+                          IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                          IMPORTED_LOCATION ${PDFIUM_LIBRARY})
+  endif()
+endif()
--- cmake/modules/packages/FindPoppler.cmake
+++ cmake/modules/packages/FindPoppler.cmake
@@ -11,7 +11,7 @@
 # Try to find Poppler.
 #
 # This is a component-based find module, which makes use of the COMPONENTS
-# and OPTIONAL_COMPONENTS arguments to find_module.  The following components
+# and OPTIONAL_COMPONENTS arguments to find_package.  The following components
 # are available::
 #
 #   Cpp  Qt5  Qt4 Glib
@@ -30,12 +30,12 @@ find_package(PkgConfig QUIET)
 if(PKG_CONFIG_FOUND)
     pkg_check_modules(PC_Poppler QUIET poppler)
 endif()
-find_path(Poppler_INCLUDE_DIR NAMES "poppler-config.h" "cpp/poppler-version.h" "qt5/poppler-qt5.h" "qt4/poppler-qt4.h"
+find_path(POPPLER_INCLUDE_DIR NAMES "poppler-config.h" "cpp/poppler-version.h" "qt5/poppler-qt5.h" "qt4/poppler-qt4.h"
           "glib/poppler.h"
           HINTS ${PC_Poppler_INCLUDE_DIRS}
           PATH_SUFFIXES poppler)
 
-find_library(Poppler_LIBRARY
+find_library(POPPLER_LIBRARY
              NAMES poppler
              HINTS ${PC_Poppler_LIBRARY_DIRS}
              )
@@ -62,37 +62,37 @@ set(Poppler_Qt4_header "poppler-qt4.h")
 set(Poppler_Glib_header "poppler.h")
 
 foreach(_comp IN LISTS Poppler_FIND_COMPONENTS)
-  set(Poppler_${_comp}_FOUND FALSE)
+  set(POPPLER_${_comp}_FOUND FALSE)
 endforeach()
 
 foreach(_comp IN LISTS Poppler_known_components)
     list(FIND Poppler_FIND_COMPONENTS "${_comp}" _nextcomp)
     if(_nextcomp GREATER -1)
-        find_path(Poppler_${_comp}_INCLUDE_DIR
+        find_path(POPPLER_${_comp}_INCLUDE_DIR
                   NAMES ${Poppler_${_comp}_header}
                   PATH_SUFFIXES poppler
                   HINTS ${PC_Poppler_${_comp}_INCLUDE_DIRS}
                   )
-        find_library(Poppler_${_comp}_LIBRARY
+        find_library(POPPLER_${_comp}_LIBRARY
                 NAMES ${Poppler_${_comp}_lib}
                 HINTS ${PC_Poppler_${_comp}_LIBRARY_DIRS}
         )
     endif()
 endforeach()
 
-if(Poppler_INCLUDE_DIR AND Poppler_LIBRARY)
+if(POPPLER_INCLUDE_DIR AND POPPLER_LIBRARY)
     if(PC_Poppler_VERSION)
-        set(Poppler_VERSION ${PC_Poppler_VERSION})
+        set(POPPLER_VERSION ${PC_Poppler_VERSION})
     endif()
-    if(NOT Poppler_VERSION)
-        find_file(Poppler_VERSION_HEADER
+    if(NOT POPPLER_VERSION)
+        find_file(POPPLER_VERSION_HEADER
             NAMES "poppler-config.h" "cpp/poppler-version.h"
             HINTS ${Poppler_INCLUDE_DIR}
             PATH_SUFFIXES poppler
         )
-        mark_as_advanced(Poppler_VERSION_HEADER)
-        if(Poppler_VERSION_HEADER)
-            file(READ ${Poppler_VERSION_HEADER} _poppler_version_header_contents)
+        mark_as_advanced(POPPLER_VERSION_HEADER)
+        if(POPPLER_VERSION_HEADER)
+            file(READ ${POPPLER_VERSION_HEADER} _poppler_version_header_contents)
             string(REGEX REPLACE
                 "^.*[ \t]+POPPLER_VERSION[ \t]+\"([0-9d.]*)\".*$"
                 "\\1"
@@ -103,57 +103,52 @@ if(Poppler_INCLUDE_DIR AND Poppler_LIBRARY)
         endif()
     endif()
 
-    if(NOT Poppler_VERSION)
+    if(NOT POPPLER_VERSION)
         # Check that we can accept Page::pageObj private member
         set(POPPLER_TEST_SOURCE "#define private public\n#include <poppler/Page.h>\n#include <poppler/splash/SplashBitmap.h>\n
         int main(int argc, char** argv) { return &(((Page*)0x8000)->pageObj) == 0; }")
         check_cxx_source_compiles(POPPLER_TEST_SOURCE   TEST_POPPLER_PAGEOBJ)
         if(TEST_POPPLER_PAGEOBJ)
             set(HAVE_POPPLER TRUE)
-            set(Poppler_VERSION "0.58")
+            set(POPPLER_VERSION "0.58")
         else()
             # poppler 0.23.0 needs to be linked against pthread
-            set (CMAKE_REQUIRED_FLAGS "${CMAKE_CXX_FLAGS} -lpthread -I${Poppler_INCLUDE_DIR}")
+            set (CMAKE_REQUIRED_FLAGS "${CMAKE_CXX_FLAGS} -lpthread -I${POPPLER_INCLUDE_DIR}")
             check_cxx_source_compiles(POPPLER_TEST_SOURCE TEST_POPPLER_PTHREAD)
             set(CMAKE_REQUIRED_FLAGS "")
             if(TEST_POPPLER_PTHREAD)
                 set(HAVE_POPPLER TRUE)
-                set(Poppler_VERSION "0.23")
+                set(POPPLER_VERSION "0.23")
             else()
-                set(Poppler_VERSION "0.20")
+                set(POPPLER_VERSION "0.20")
             endif()
         endif()
     endif()
 endif()
 
 find_package_handle_standard_args(Poppler
-                                  FOUND_VAR Poppler_FOUND
-                                  REQUIRED_VARS Poppler_LIBRARY Poppler_INCLUDE_DIR
-                                  VERSION_VAR  Poppler_VERSION
-                                  HANDLE_COMPONENTS
-                                  )
+                                  REQUIRED_VARS POPPLER_LIBRARY POPPLER_INCLUDE_DIR
+                                  VERSION_VAR  POPPLER_VERSION
+                                  HANDLE_COMPONENTS)
 
-mark_as_advanced(
-    Poppler_INCLUDE_DIR
-    Poppler_LIBRARY
-)
+mark_as_advanced(Poppler_INCLUDE_DIR Poppler_LIBRARY)
 
-if(Poppler_FOUND)
-    set(Poppler_INCLUDE_DIRS ${Poppler_INCLUDE_DIR})
-    set(Poppler_LIBRARIES ${Poppler_LIBRARY})
+if(POPPLER_FOUND)
+    set(POPPLER_INCLUDE_DIRS ${POPPLER_INCLUDE_DIR})
+    set(POPPLER_LIBRARIES ${POPPLER_LIBRARY})
 
     if(NOT TARGET POPPLER::Poppler)
         add_library(POPPLER::Poppler UNKNOWN IMPORTED)
         set_target_properties(POPPLER::Poppler PROPERTIES
-                              INTERFACE_INCLUDE_DIRECTORIES ${Poppler_INCLUDE_DIR}
+                              INTERFACE_INCLUDE_DIRECTORIES ${POPPLER_INCLUDE_DIR}
                               IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-                              IMPORTED_LOCATION "${Poppler_LIBRARY}")
+                              IMPORTED_LOCATION "${POPPLER_LIBRARY}")
         foreach(tgt IN LISTS Poppler_known_components)
             add_library(POPPLER::${tgt} UNKNOWN IMPORTED)
             set_target_properties(POPPLER::${tgt} PROPERTIES
-                                  INTERFACE_INCLUDE_DIRECTORIES ${Poppler_${tgt}_INCLUDE_DIR}
+                                  INTERFACE_INCLUDE_DIRECTORIES ${POPPLER_${tgt}_INCLUDE_DIR}
                                   IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-                                  IMPORTED_LOCATION ${Poppler_${tgt}_LIBRARY})
+                                  IMPORTED_LOCATION ${POPPLER_${tgt}_LIBRARY})
         endforeach()
     endif()
 endif()
