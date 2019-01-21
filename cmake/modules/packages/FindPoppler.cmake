ff8b3dd34e268fe308c84b103f37b788bd4e224c
Hiroshi Miura <miurahr@linux.com>
2019-01-21 22:46:42 +0900

    cmake: Improve FindPoppler
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- cmake/modules/packages/FindPoppler.cmake
+++ cmake/modules/packages/FindPoppler.cmake
@@ -1,56 +1,65 @@
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 # file COPYING-CMAKE-SCRIPTS or https://cmake.org/licensing for details.
 
-#.rst:
-# FindPoppler
-# -----------
-#
-# Copyright (c) 2017, Hiroshi Miura <miurahr@linux.com>
-# Copyright (c) 2015, Alex Richardson <arichardson.kde@gmail.com>
-#
-# Try to find Poppler.
-#
-# This is a component-based find module, which makes use of the COMPONENTS
-# and OPTIONAL_COMPONENTS arguments to find_package.  The following components
-# are available::
-#
-#   Cpp  Qt5  Qt4 Glib
-#
-# If it's found it sets POPPLER_FOUND to TRUE
-# and following variables are set:
-#    POPPLER_INCLUDE_DIRS
-#    POPPLER_LIBRARY
-#    POPPLER_VERSION
-#
-include(FeatureSummary)
-include(FindPackageHandleStandardArgs)
-include(CheckCXXSourceCompiles)
+#[=======================================================================[.rst:
+FindPoppler
+--------
+
+Find the Poppler headers and libraries.
+
+This is a component-based find module, which makes use of the COMPONENTS
+and OPTIONAL_COMPONENTS arguments to find_package.  The following components
+are available::
+
+  Cpp  Qt5  Qt4  Glib
+
+Copyright (c) 2017,2019, Hiroshi Miura <miurahr@linux.com>
+Copyright (c) 2015, Alex Richardson <arichardson.kde@gmail.com>
+
+IMPORTED Targets
+^^^^^^^^^^^^^^^^
+
+This module defines :prop_tgt:`IMPORTED` target ``POPPLER::Poppler``, if
+curl has been found.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This module defines the following variables:
+
+``POPPLER_FOUND``
+  True if Poppler found.
+
+``POPPLER_INCLUDE_DIRS``
+  where to find poppler/poppler-config.h, etc.
+
+``POPPLER_LIBRARIES``
+  List of libraries when using Poppler.
+
+``POPPLER_VERSION_STRING``
+  The version of Poppler found.
+#]=======================================================================]
 
 find_package(PkgConfig QUIET)
 if(PKG_CONFIG_FOUND)
-    pkg_check_modules(PC_Poppler QUIET poppler)
+  pkg_check_modules(PC_Poppler QUIET poppler)
+  if(PC_Poppler_VERSION)
+    set(POPPLER_VERSION_STRING ${PC_Poppler_VERSION})
+  endif()
 endif()
 find_path(POPPLER_INCLUDE_DIR NAMES "poppler-config.h" "cpp/poppler-version.h" "qt5/poppler-qt5.h" "qt4/poppler-qt4.h"
           "glib/poppler.h"
           HINTS ${PC_Poppler_INCLUDE_DIRS}
           PATH_SUFFIXES poppler)
 
-find_library(POPPLER_LIBRARY
-             NAMES poppler
-             HINTS ${PC_Poppler_LIBRARY_DIRS}
-             )
+find_library(POPPLER_LIBRARY NAMES poppler HINTS ${PC_Poppler_LIBRARY_DIRS})
 
-set(Poppler_known_components
-    Cpp
-    Qt4
-    Qt5
-    Glib
-)
+set(Poppler_known_components Cpp Qt4 Qt5 Glib )
 foreach(_comp IN LISTS Poppler_known_components)
-    string(TOLOWER "${_comp}" _lc_comp)
-    set(Poppler_${_comp}_pkg_config "poppler-${_lc_comp}")
-    set(Poppler_${_comp}_lib "poppler-${_lc_comp}")
-    set(Poppler_${_comp}_header_subdir "poppler/${_lc_comp}")
+  string(TOLOWER "${_comp}" _lc_comp)
+  set(Poppler_${_comp}_pkg_config "poppler-${_lc_comp}")
+  set(Poppler_${_comp}_lib "poppler-${_lc_comp}")
+  set(Poppler_${_comp}_header_subdir "poppler/${_lc_comp}")
 endforeach()
 set(Poppler_known_components Core ${Poppler_known_components})
 
@@ -66,94 +75,62 @@ foreach(_comp IN LISTS Poppler_FIND_COMPONENTS)
 endforeach()
 
 foreach(_comp IN LISTS Poppler_known_components)
-    list(FIND Poppler_FIND_COMPONENTS "${_comp}" _nextcomp)
-    if(_nextcomp GREATER -1)
-        find_path(POPPLER_${_comp}_INCLUDE_DIR
-                  NAMES ${Poppler_${_comp}_header}
-                  PATH_SUFFIXES poppler
-                  HINTS ${PC_Poppler_${_comp}_INCLUDE_DIRS}
-                  )
-        find_library(POPPLER_${_comp}_LIBRARY
-                NAMES ${Poppler_${_comp}_lib}
-                HINTS ${PC_Poppler_${_comp}_LIBRARY_DIRS}
-        )
-    endif()
+  list(FIND Poppler_FIND_COMPONENTS "${_comp}" _nextcomp)
+  if(_nextcomp GREATER -1)
+    find_path(POPPLER_${_comp}_INCLUDE_DIR
+              NAMES ${Poppler_${_comp}_header}
+              PATH_SUFFIXES poppler
+              HINTS ${PC_Poppler_${_comp}_INCLUDE_DIRS})
+    find_library(POPPLER_${_comp}_LIBRARY
+            NAMES ${Poppler_${_comp}_lib}
+            HINTS ${PC_Poppler_${_comp}_LIBRARY_DIRS})
+  endif()
 endforeach()
 
-if(POPPLER_INCLUDE_DIR AND POPPLER_LIBRARY)
-    if(PC_Poppler_VERSION)
-        set(POPPLER_VERSION ${PC_Poppler_VERSION})
-    endif()
-    if(NOT POPPLER_VERSION)
-        find_file(POPPLER_VERSION_HEADER
+if(NOT POPPLER_VERSION_STRING)
+  find_file(POPPLER_VERSION_HEADER
             NAMES "poppler-config.h" "cpp/poppler-version.h"
-            HINTS ${Poppler_INCLUDE_DIR}
+            HINTS ${POPPLER_INCLUDE_DIR}
             PATH_SUFFIXES poppler
-        )
-        mark_as_advanced(POPPLER_VERSION_HEADER)
-        if(POPPLER_VERSION_HEADER)
-            file(READ ${POPPLER_VERSION_HEADER} _poppler_version_header_contents)
-            string(REGEX REPLACE
-                "^.*[ \t]+POPPLER_VERSION[ \t]+\"([0-9d.]*)\".*$"
-                "\\1"
-                Poppler_VERSION
-                "${_poppler_version_header_contents}"
             )
-            unset(_poppler_version_header_contents)
-        endif()
-    endif()
-
-    if(NOT POPPLER_VERSION)
-        # Check that we can accept Page::pageObj private member
-        set(POPPLER_TEST_SOURCE "#define private public\n#include <poppler/Page.h>\n#include <poppler/splash/SplashBitmap.h>\n
-        int main(int argc, char** argv) { return &(((Page*)0x8000)->pageObj) == 0; }")
-        check_cxx_source_compiles(POPPLER_TEST_SOURCE   TEST_POPPLER_PAGEOBJ)
-        if(TEST_POPPLER_PAGEOBJ)
-            set(HAVE_POPPLER TRUE)
-            set(POPPLER_VERSION "0.58")
-        else()
-            # poppler 0.23.0 needs to be linked against pthread
-            set (CMAKE_REQUIRED_FLAGS "${CMAKE_CXX_FLAGS} -lpthread -I${POPPLER_INCLUDE_DIR}")
-            check_cxx_source_compiles(POPPLER_TEST_SOURCE TEST_POPPLER_PTHREAD)
-            set(CMAKE_REQUIRED_FLAGS "")
-            if(TEST_POPPLER_PTHREAD)
-                set(HAVE_POPPLER TRUE)
-                set(POPPLER_VERSION "0.23")
-            else()
-                set(POPPLER_VERSION "0.20")
-            endif()
-        endif()
-    endif()
+  if(POPPLER_VERSION_HEADER)
+    file(READ ${POPPLER_VERSION_HEADER} _poppler_version_header_contents)
+    string(REGEX REPLACE
+           "^.*[ \t]+POPPLER_VERSION[ \t]+\"([0-9d.]*)\".*$"
+           "\\1"
+           POPPLER_VERSION_STRING
+           "${_poppler_version_header_contents}"
+           )
+    unset(_poppler_version_header_contents)
+  endif()
 endif()
 
+include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Poppler
                                   REQUIRED_VARS POPPLER_LIBRARY POPPLER_INCLUDE_DIR
-                                  VERSION_VAR  POPPLER_VERSION
+                                  VERSION_VAR  POPPLER_VERSION_STRING
                                   HANDLE_COMPONENTS)
-
-mark_as_advanced(Poppler_INCLUDE_DIR Poppler_LIBRARY)
+mark_as_advanced(POPPLER_INCLUDE_DIR POPPLER_LIBRARY)
 
 if(POPPLER_FOUND)
-    set(POPPLER_INCLUDE_DIRS ${POPPLER_INCLUDE_DIR})
-    set(POPPLER_LIBRARIES ${POPPLER_LIBRARY})
-
-    if(NOT TARGET POPPLER::Poppler)
-        add_library(POPPLER::Poppler UNKNOWN IMPORTED)
-        set_target_properties(POPPLER::Poppler PROPERTIES
-                              INTERFACE_INCLUDE_DIRECTORIES ${POPPLER_INCLUDE_DIR}
-                              IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-                              IMPORTED_LOCATION "${POPPLER_LIBRARY}")
-        foreach(tgt IN LISTS Poppler_known_components)
-            add_library(POPPLER::${tgt} UNKNOWN IMPORTED)
-            set_target_properties(POPPLER::${tgt} PROPERTIES
-                                  INTERFACE_INCLUDE_DIRECTORIES ${POPPLER_${tgt}_INCLUDE_DIR}
-                                  IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-                                  IMPORTED_LOCATION ${POPPLER_${tgt}_LIBRARY})
-        endforeach()
-    endif()
+  set(POPPLER_INCLUDE_DIRS ${POPPLER_INCLUDE_DIR})
+  set(POPPLER_LIBRARIES ${POPPLER_LIBRARY})
+  if(NOT TARGET POPPLER::Poppler)
+    add_library(POPPLER::Poppler UNKNOWN IMPORTED)
+    set_target_properties(POPPLER::Poppler PROPERTIES
+                          INTERFACE_INCLUDE_DIRECTORIES ${POPPLER_INCLUDE_DIR}
+                          IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                          IMPORTED_LOCATION "${POPPLER_LIBRARY}")
+    foreach(tgt IN LISTS Poppler_known_components)
+      add_library(POPPLER::${tgt} UNKNOWN IMPORTED)
+      set_target_properties(POPPLER::${tgt} PROPERTIES
+                            INTERFACE_INCLUDE_DIRECTORIES ${POPPLER_${tgt}_INCLUDE_DIR}
+                            IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+                            IMPORTED_LOCATION ${POPPLER_${tgt}_LIBRARY})
+    endforeach()
+  endif()
 endif()
 
+include(FeatureSummary)
 set_package_properties(Poppler PROPERTIES
-    DESCRIPTION "A PDF rendering library"
-    URL "http://poppler.freedesktop.org"
-)
+                       DESCRIPTION "A PDF rendering library" URL "http://poppler.freedesktop.org")
